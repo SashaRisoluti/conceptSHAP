a3f79faee578646397d83c4811123a8f5f3387da
SashaRisoluti <164863971+SashaRisoluti@users.noreply.github.com>
2024-07-22 11:30:31 +0200

    Update skill-dataloader.sh

--- data/skill-dataloader.sh
+++ data/skill-dataloader.sh
@@ -1,5 +1,5 @@
 python3 data/skill-dataloader.py \
     --download_dir="/kaggle/input/sample-skill-extraction/skill_train.pkl" \
     --size=1000 \
-    --run_option=3 \
+    --run_option=1 \
     --train_dir="/kaggle/working//skill-fragments.pkl"
